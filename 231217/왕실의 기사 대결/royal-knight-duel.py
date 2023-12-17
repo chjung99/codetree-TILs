0a243b2dc42a68951a5ffb09f0fddd5511872d48
chjung99 <62923434+chjung99@users.noreply.github.com>
2023-12-17 17:34:58 +0900

    Title: 왕실의 기사 대결, Time: 394ms, Memory: 28MB, Status: Passed - CodeTree

--- 231217/왕실의 기사 대결/royal-knight-duel.py
+++ 231217/왕실의 기사 대결/royal-knight-duel.py
@@ -25,31 +25,51 @@ for order in orders:
     if knights[i - 1][-2] == 0:  # 사라진 기사
         continue
 
+    vis = [[0 for _ in range(L)] for __ in range(L)]
     group = [[0 for _ in range(L)] for __ in range(L)]
-    new_board = [[0 for _ in range(L)] for __ in range(L)]
+    new_board = [[-1 for _ in range(L)] for __ in range(L)]
 
     # new_board = 기사 영역
-    for knight in knights:
+    for idx, knight in enumerate(knights):
         if knight[-2] == 0:
             continue
         nx, ny, h, w = knight[:-2]
         for j in range(h):
             for k in range(w):
-                new_board[nx + j][ny + k] = 1
+                new_board[nx + j][ny + k] = idx
+
     # 명령 받은 기사와 붙어 있는 그룹 확인
-    kx, ky = knights[i - 1][:2]
-    queue = deque([(kx, ky)])
-    group[kx][ky] = 1
+    kx, ky, kh, kw = knights[i - 1][:4]
+    queue = deque([])
+
+    for j in range(kh):
+        for k in range(kw):
+            queue.append((kx + j, ky + k, i-1))
+            vis[kx + j][ky + k] = 1
+            group[kx + j][ky + k] = 1
+    # for b in new_board:
+    #     print(b)
 
     while queue:
-        cx, cy = queue.pop()
-        for j in range(4):
-            nx, ny = cx + dx[j], cy + dy[j]
-            if nx >= L or nx < 0 or ny >= L or ny < 0:
-                continue
-            if group[nx][ny] == 0 and new_board[nx][ny]:
-                group[nx][ny] = 1
-                queue.append((nx, ny))
+        cx, cy, c_idx = queue.pop()
+        nx, ny = cx + dx[d], cy + dy[d]
+        if nx >= L or nx < 0 or ny >= L or ny < 0 or vis[nx][ny]:
+            continue
+        n_idx = new_board[nx][ny]
+        # print(nx,ny,n_idx)
+        if n_idx != -1:
+            # print("!")
+            nkx, nky, nkh, nkw = knights[n_idx][:4]
+
+            for j in range(nkh):
+                for k in range(nkw):
+                    queue.append((nkx + j, nky + k, n_idx))
+                    vis[nkx + j][nky + k] = 1
+                    group[nkx+j][nky+k] = 1
+
+    #
+    # for g in group:
+    #     print(g)
 
     # 벽이 있는 지 확인
     is_block = False
@@ -58,26 +78,31 @@ for order in orders:
             if group[j][k]:
                 nx, ny = j + dx[d], k + dy[d]
                 if nx >= L or nx < 0 or ny >= L or ny < 0 or boards[nx][ny] == 2:
+
                     is_block = True
                     break
         if is_block:
             break
 
     # 벽이 없으면 그룹 이동 & 데미지 계산
+    # print(is_block)
     if not is_block:
         for j in range(len(knights)):
             if group[knights[j][0]][knights[j][1]] and knights[j][-2]:
                 knights[j][0] += dx[d]
                 knights[j][1] += dy[d]
 
-                if (kx+dx[d], ky+dy[d]) != (knights[j][0], knights[j][1]):
+                if (kx + dx[d], ky + dy[d]) != (knights[j][0], knights[j][1]):
                     for k in range(knights[j][2]):
                         for l in range(knights[j][3]):
                             if knights[j][-2] > 0 and boards[knights[j][0] + k][knights[j][1] + l] == 1:
                                 knights[j][-2] -= 1
                                 knights[j][-1] += 1
+    # for knight in knights:
+    #     print(knights)
+    # print("====")
 ans = 0
 for knight in knights:
     if knight[-2]:
-        ans+=knight[-1]
+        ans += knight[-1]
 print(ans)
