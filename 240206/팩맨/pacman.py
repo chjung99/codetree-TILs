d609340552c51ccd661cbecb7f79fa1264c75d21
chjung99 <62923434+chjung99@users.noreply.github.com>
2024-02-06 22:09:34 +0900

    Title: 팩맨, Time: 204ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240206/팩맨/pacman.py
+++ 240206/팩맨/pacman.py
@@ -1,69 +1,84 @@
 from collections import deque
+from copy import deepcopy
+
+m, t = map(int, input().split())
+px, py = map(int, input().split())
+
+monsters = [[[0 for _ in range(8)] for __ in range(5)] for ___ in range(5)]
+dead_monsters = [[0 for __ in range(5)] for ___ in range(5)]
+ttl_monsters = deque([])
+eggs = [[[0 for _ in range(8)] for __ in range(5)] for ___ in range(5)]
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, -1, -1, -1, 0, 1, 1, 1]
+
+for _ in range(m):
+    x, y, d = map(int, input().split())
+    monsters[x][y][d - 1] += 1
 
 
-def rotate45(self):
-    self.direction = (self.direction + 1) % 8
+def get_alive_monsters():
+    cnt = 0
+    for i in range(1, 5):
+        for j in range(1, 5):
+            for k in range(8):
+                cnt += monsters[i][j][k]
+    return cnt
 
 
-def move(mon, next_step):
-    x, y, d, alive, ttl = mon
-    board[x][y] -= 1
-    x, y = next_step
+def make_eggs():
+    global eggs
+    for i in range(1, 5):
+        for j in range(1, 5):
+            for k in range(8):
+                eggs[i][j][k] = monsters[i][j][k]
 
-    board[x][y] += 1
 
-
-def find_next_step(x, y, d):
-    can_move = False
-    nx, ny = x + dx[d], y + dy[d]
-    if is_out_board(nx, ny) or is_dead_monster(nx, ny) or is_packman(nx, ny):
-        d = (d + 1) % 8
-        for _ in range(7):
-            nx, ny = x + dx[d], y + dy[d]
-            if not is_out_board(nx, ny) and not is_dead_monster(nx, ny) and not is_packman(nx, ny):
-                can_move = True
-                break
-            d = (d + 1) % 8
-    else:
-        can_move = True
-    if not can_move:
-        return [x, y, d]
-    else:
-        return [nx, ny, d]
+def wake_eggs():
+    for i in range(1, 5):
+        for j in range(1, 5):
+            for k in range(8):
+                monsters[i][j][k] += eggs[i][j][k]
 
 
 def is_out_board(nx, ny):
     return nx < 1 or nx > 4 or ny < 1 or ny > 4
 
 
-def is_dead_monster(nx, ny):
-    return ghost_board[nx][ny] > 0
-
-
 def is_packman(nx, ny):
-    return nx == px and ny == py
+    return px == nx and py == ny
 
 
-def make_monster_eggs():
-    for x, y, d, alive, ttl in monsters:
-        if alive:
-            egg = [x, y, d, False, -1]
-            eggs.append(egg)
+def is_dead_monster(nx, ny):
+    return dead_monsters[nx][ny] > 0
+
+
+def find_next_step(cx, cy, cd):
+    nx, ny, nd = cx + dx[cd], cy + dy[cd], cd
+    cnt = 7
+    while cnt:
+        if not is_out_board(nx, ny) and not is_packman(nx, ny) and not is_dead_monster(nx, ny):
+            return nx, ny, nd
+        nd = (nd + 1) % 8
+        nx, ny, nd = cx + dx[nd], cy + dy[nd], nd
+        cnt -= 1
+    return cx, cy, cd
 
 
 def move_monsters():
-    for i, (x, y, d, alive, ttl) in enumerate(monsters):
-        if alive:
-            nx, ny, nd = find_next_step(x, y, d)
-            board[x][y] -= 1
+    global monsters
+    tmp_monsters = deepcopy(monsters)
+    for i in range(1, 5):
+        for j in range(1, 5):
+            for k in range(8):
+                cnt = monsters[i][j][k]
+                if cnt > 0:
+                    nx, ny, nd = find_next_step(i, j, k)
+                    tmp_monsters[i][j][k] -= cnt
+                    tmp_monsters[nx][ny][nd] += cnt
+    monsters = tmp_monsters
 
-            board[nx][ny] += 1
-            monsters[i][0] = nx
-            monsters[i][1] = ny
-            monsters[i][2] = nd
 
-
-def find_best_path(cx, cy):
+def find_next_path(cx, cy):
     result = []  # [몬스터수, "123"]
     for i in range(1, 5):
         for j in range(1, 5):
@@ -79,7 +94,7 @@ def find_best_path(cx, cy):
                 if is_out_board(nx, ny):
                     continue
                 else:
-                    cnt += board[nx][ny]
+                    cnt += sum(monsters[nx][ny])
                     eat.append([nx, ny])
 
                 nx, ny = nx + dx[j_dir], ny + dy[j_dir]
@@ -88,7 +103,7 @@ def find_best_path(cx, cy):
                     continue
                 else:
                     if [nx, ny] not in eat:
-                        cnt += board[nx][ny]
+                        cnt += sum(monsters[nx][ny])
                         eat.append([nx, ny])
 
                 nx, ny = nx + dx[k_dir], ny + dy[k_dir]
@@ -97,92 +112,59 @@ def find_best_path(cx, cy):
                     continue
                 else:
                     if [nx, ny] not in eat:
-                        cnt += board[nx][ny]
+                        cnt += sum(monsters[nx][ny])
                 result.append([cnt, str(i) + str(j) + str(k)])
 
     result.sort(key=lambda x: (-x[0], x[1]))
     return result[0][-1]
 
 
-def move_and_eat(path, cur_turn):  # path = "123"
+def move_and_eat(next_path, cur_turn):
     global px, py
-    for str_dir in path:
+    for str_dir in next_path:
         cur_dir = (int(str_dir) - 1) * 2
 
         px, py = px + dx[cur_dir], py + dy[cur_dir]
-
-        for i, (x, y, d, alive, ttl) in enumerate(monsters):
-            if alive:
-                if [x, y] == [px, py]:
-                    monsters[i][3] = False
-                    monsters[i][4] = turn + 2
-                    board[px][py] -= 1
-                    ghost_board[px][py] += 1
+        # 몬스터 잡기
+        num_of_deads = sum(monsters[px][py])
+        dead_monsters[px][py] += num_of_deads
+        ttl_monsters.append([cur_turn + 2, px, py, num_of_deads])
+        for k in range(8):
+            monsters[px][py][k] = 0
 
 
-def move_packman(x, y, turn):
-    path = find_best_path(x, y)
-    # print(x, y , path)
-    move_and_eat(path, turn)
+def move_packman(cur_turn):
+    next_path = find_next_path(px, py)
+    # print(px, py, next_path)
+    move_and_eat(next_path, cur_turn)
 
 
 def clear_dead_monsters(cur_turn):
-    global monsters
-    new_monsters = deque([])
-    while monsters:
-        mon = monsters.popleft()
-        x, y, d, alive, ttl = mon
-        if not alive and ttl == cur_turn:
-            if ghost_board[x][y] > 0:
-                ghost_board[x][y] -= 1
-            continue
-        new_monsters.append(mon)
-    monsters = new_monsters
+    head = ttl_monsters.popleft()
+    while head[0] == cur_turn:
+        cx, cy, num_of_deads = head[1:]
+        dead_monsters[cx][cy] -= num_of_deads
+        head = ttl_monsters.popleft()
+    if head[0] != cur_turn:
+        ttl_monsters.appendleft(head)
 
 
-def wake_monster_eggs():
-    while eggs:
-        egg = eggs.popleft()
-        monster = [egg[0], egg[1], egg[2], True, -1]
-        board[egg[0]][egg[1]] += 1
-        monsters.append(monster)
-
-
-def get_alive_monsters():
-    cnt = 0
-    for i in range(1, 5):
-        for j in range(1, 5):
-            cnt += board[i][j]
-    return cnt
-
-
-m, t = map(int, input().split())
-px, py = map(int, input().split())  # 1,1 ~ 4,4
-monsters = deque([])
-eggs = deque([])
-board = [[0 for _ in range(5)] for __ in range(5)]  # 1,1 ~ 4.4
-ghost_board = [[0 for _ in range(5)] for __ in range(5)]  # 1,1 ~ 4.4
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
-dy = [0, -1, -1, -1, 0, 1, 1, 1]
-
-for _ in range(m):
-    mx, my, md = map(int, input().split())
-    monster = [mx, my, md - 1, True, -1]
-    board[mx][my] += 1
-    monsters.append(monster)
-
-for turn in range(t):  # 25
-    make_monster_eggs()  # 100 0000
-    move_monsters()  # 7 * 100 0000 * 100 0000
-    # for mon in monsters:
-    #     if mon[3]:
-    #         print(mon, end=" ")
+for turn in range(1, t + 1):
+    make_eggs()
+    # for i in range(1, 5):
+    #     for j in range(1, 5):
+    #         print(sum(monsters[i][j]), end=" ")
+    #     print()
+    move_monsters()
+    # print("===move===")
+    # for i in range(1, 5):
+    #     for j in range(1, 5):
+    #         print(sum(monsters[i][j]), end=" ")
+    #     print()
     # print()
-    move_packman(px, py, turn)  # 1 * 100 0000 + 3 * 100 0000
-    clear_dead_monsters(turn)  # 100 0000
-    wake_monster_eggs()  # 100 0000
-
-
+    move_packman(turn)
+    clear_dead_monsters(turn)
+    wake_eggs()
 
 print(get_alive_monsters())
 '''
