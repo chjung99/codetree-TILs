91c512a5ee375b3e15c0dac07e99788efc06a3c4
chjung99 <62923434+chjung99@users.noreply.github.com>
2024-10-01 22:17:15 +0900

    Title: 마법의 숲 탐색, Time: 525ms, Memory: 29MB, Status: Passed - CodeTree

--- 241001/마법의 숲 탐색/magical-forest-exploration.py
+++ 241001/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,180 +1,154 @@
 from collections import deque
-from xmlrpc.client import FastParser
 
-R, C, K = map(int ,input().split())
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+def can_move_to_south(cx, cy, board):
+    # 이동방향에 board가 1이 아니면 됨 + 좌우 범위 C
+    dd_x, dd_y = cx + 2, cy
+    dl_x, dl_y = cx + 1, cy - 1
+    dr_x, dr_y = cx + 1, cy + 1
 
-def out_of_range(x, y):
-    return x < 0 or x > R or y < 1 or y > C
+    if (dd_x <= R+2 and dl_x <= R+2 and dr_x <= R+2) and \
+        (1 <= dd_y <= C and 1 <= dl_y <= C and 1 <= dr_y <= C):
+        return board[dd_x][dd_y] == 0 and board[dl_x][dl_y] == 0 and \
+            board[dr_x][dr_y] == 0
 
-def can_move_to(locations, board):
-    for nx, ny in locations:
-        if out_of_range(nx, ny) or board[nx][ny]:
-            return False
-    return True
+    return False
 
-class Golam:
-    def __init__(self, x, y, d):
-        self.cx = x
-        self.cy = y
-        self.cd = d
-    def rotate_exit_clockwise(self):
-        self.cd = (self.cd + 1) % 4
-    def rotate_exit_reverse_clockwise(self):
-        self.cd = (self.cd - 1) % 4
+
+def can_move_to_west_south(cx, cy, board):
+    ll_x, ll_y = cx, cy - 2
+    lu_x, lu_y = cx - 1, cy - 1
+    ld_x, ld_y = cx + 1, cy - 1
+    if (ll_x <= R+2 and lu_x <= R+2 and ld_x <= R+2) and \
+            (1 <= ll_y <= C and 1 <= lu_y <= C and 1 <= ld_y <= C):
+        return board[ll_x][ll_y] == 0 and board[lu_x][lu_y] == 0 and \
+            board[ld_x][ld_y] == 0 and can_move_to_south(cx, cy - 1, board)
+    return False
+
+
+def can_move_to_east_south(cx, cy, board):
+    rr_x, rr_y = cx, cy + 2
+    ru_x, ru_y = cx - 1, cy + 1
+    rd_x, rd_y = cx + 1, cy + 1
+    if (rr_x <= R+2 and ru_x <= R+2 and rd_x <= R+2) and \
+            (1 <= rr_y <= C and 1 <= ru_y <= C and 1 <= rd_y <= C):
+        return board[rr_x][rr_y] == 0 and board[ru_x][ru_y] == 0 and \
+            board[rd_x][rd_y] == 0 and can_move_to_south(cx, cy + 1, board)
+    return False
+
+
+def out_of_forest(x, y):
+    return x < 3 or x > R + 2 or y < 1 or y > C
+
+
+class Golem:
+    def __init__(self, cx, cy, cd):
+        self.cx = cx
+        self.cy = cy
+        self.cd = cd
+
     def move_south(self):
-        self.cx = self.cx + 1
+        self.cx += 1
+
     def move_east(self):
-        self.cy = self.cy + 1
+        self.cy += 1
+
     def move_west(self):
-        self.cy = self.cy - 1
-    def out_ch(self):
-        return self.cx, self.cy
-    def record_on_board(self, board, num):
+        self.cy -= 1
+
+    def rotate_clockwise(self):
+        self.cd = (self.cd + 1) % 4
+
+    def rotate_anti_clockwise(self):
+        self.cd = (self.cd - 1) % 4
+
+    def add_on_board(self, board, door, num):
         board[self.cx][self.cy] = num
         for i in range(4):
             nx, ny = self.cx + dx[i], self.cy + dy[i]
             board[nx][ny] = num
-        door[self.cx+dx[self.cd]][self.cy+dy[self.cd]] = 1
-    def pop_on_board(self, board):
+            if i == self.cd:
+                door[nx][ny] = 1
+
+    def remove_on_board(self, board, door):
         board[self.cx][self.cy] = 0
         for i in range(4):
             nx, ny = self.cx + dx[i], self.cy + dy[i]
             board[nx][ny] = 0
-        door[self.cx + dx[self.cd]][self.cy + dy[self.cd]] = 0
-    def is_possible_move_south(self, board):
-        # 범위를 벗어나거나, 골렘이 있는 경우 이동 불가
-        next_locations = []
-        for i in range(4):
-            if i == 0:
-                continue
-            nx, ny = self.cx + 1 + dx[i], self.cy + dy[i]
-            next_locations.append([nx, ny])
-        return can_move_to(next_locations, board)
+            if i == self.cd:
+                door[nx][ny] = 0
 
-    def is_possible_move_east_south(self, board):
-        # 범위를 벗어나거나, 골렘이 있는 경우 이동 불가
-        next_east_locations = []
-        for i in range(4):
-            if self.cx == 0 and i == 0:
-                continue
-            if i == 3:
-                continue
-            nx, ny = self.cx + dx[i], self.cy + 1 + dy[i]
-            next_east_locations.append([nx, ny])
-
-        next_south_locations = []
-        for i in range(4):
-            if i == 0:
-                continue
-            nx, ny = self.cx + 1 + dx[i], self.cy + 1 + dy[i]
-            next_south_locations.append([nx, ny])
-
-        return can_move_to(next_east_locations, board) and \
-            can_move_to(next_south_locations, board)
-
-    def is_possible_move_west_south(self, board):
-        next_west_locations = []
-        for i in range(4):
-            if self.cx == 0 and i == 0:
-                continue
-            if i == 1:
-                continue
-            nx, ny = self.cx + dx[i], self.cy - 1 + dy[i]
-            next_west_locations.append([nx, ny])
-
-        next_south_locations = []
-        for i in range(4):
-            if i == 0:
-                continue
-            nx, ny = self.cx + 1 + dx[i], self.cy - 1 + dy[i]
-            next_south_locations.append([nx, ny])
-
-        return can_move_to(next_west_locations, board) and \
-            can_move_to(next_south_locations, board)
-
-    def is_out_of_board(self):
-        if self.cx < 1 or self.cx > R or self.cy < 1 or self.cy > C:
-            return True
-
+    def is_body_out_of_forest(self):
+        body = [[self.cx, self.cy]]
         for i in range(4):
             nx, ny = self.cx + dx[i], self.cy + dy[i]
-            if nx < 1 or nx > R or ny < 1 or ny > C:
+            body.append([nx, ny])
+        for bx, by in body:
+            if out_of_forest(bx, by):
                 return True
         return False
-golams = []
-board = [[0 for _ in range(C+1)]for __ in range(R+1)]
-door = [[0 for _ in range(C+1)]for __ in range(R+1)]
+
+
+R, C, K = map(int, input().split())
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# x = [3, R+2] = [1, R]
+# 0, 1, 2는 준비 칸 3부터 첫번째 행 -> 답은 -2해주기
+# 이동하기 위한 범위 -> 0 <= x <= R+2
+# 보드 위에 있기 위한 범위 -> 3 <= x <= R+2
+board = [[0 for _ in range(C+1)] for __ in range(R + 3)]
+door = [[0 for _ in range(C+1)] for __ in range(R + 3)]
+
+golems = []
+golems_on_board = []
 ans = 0
-cur_golams = []
+
 for _ in range(K):
-    ci, di = map(int,input().split())
-    golams.append(Golam(0, ci, di))
+    ci, di = map(int, input().split())
+    golems.append(Golem(1, ci, di))
 
 
-def bfs(cx, cy, cn, board):
-    visit = [[0 for _ in range(C+1)]for __ in range(R+1)]
+def bfs(cx, cy, board, door):
+    fx = cx - 2
+    visit = [[0 for _ in range(C+1)] for __ in range(R + 3)]
+    queue = deque([[cx, cy]])
     visit[cx][cy] = 1
-    queue = deque([[cx, cy, cn]])
-    fx, fy = cx, cy
     while queue:
-        cx, cy, cn = queue.popleft()
+        cx, cy = queue.popleft()
         for i in range(4):
             nx, ny = cx + dx[i], cy + dy[i]
-            if out_of_range(nx, ny) or visit[nx][ny]:
+            if out_of_forest(nx, ny) or visit[nx][ny] or board[nx][ny]==0:
                 continue
-            nn = board[nx][ny]
-            if door[cx][cy] == 0:
-                if cn == nn:
-                    visit[nx][ny] = 1
-                    queue.append([nx, ny, nn])
-                    if fx < nx:
-                        fx, fy = nx, ny
-            else:
-                if nn != 0:
-                    visit[nx][ny] = 1
-                    queue.append([nx, ny, nn])
-                    if fx < nx:
-                        fx, fy = nx, ny
+            if board[cx][cy] == board[nx][ny] or door[cx][cy]:
+                visit[nx][ny] = 1
+                fx = max(fx, nx-2)
+                queue.append([nx, ny])
+    return fx
 
-    return fx, fy
 
 
 for i in range(K):
     while True:
-        if golams[i].is_possible_move_south(board):
-            golams[i].move_south()
-        elif golams[i].is_possible_move_west_south(board):
-            golams[i].move_west()
-            golams[i].rotate_exit_reverse_clockwise()
-            golams[i].move_south()
-        elif golams[i].is_possible_move_east_south(board):
-            golams[i].move_east()
-            golams[i].rotate_exit_clockwise()
-            golams[i].move_south()
+        if can_move_to_south(golems[i].cx, golems[i].cy, board):
+            golems[i].move_south()
+        elif can_move_to_west_south(golems[i].cx, golems[i].cy, board):
+            golems[i].move_west()
+            golems[i].rotate_anti_clockwise()
+            golems[i].move_south()
+        elif can_move_to_east_south(golems[i].cx, golems[i].cy, board):
+            golems[i].move_east()
+            golems[i].rotate_clockwise()
+            golems[i].move_south()
         else:
             break
-
-    if golams[i].is_out_of_board():
-        for g in cur_golams:
-            g.pop_on_board(board)
-        cur_golams.clear()
-        continue
+    if golems[i].is_body_out_of_forest():
+        for g in golems_on_board:
+            g.remove_on_board(board, door)
+        golems_on_board.clear()
     else:
-        golams[i].record_on_board(board, i+1)
-        cur_golams.append(golams[i])
-        cx, cy = golams[i].out_ch()
-        cn = board[cx][cy]
-        fx, fy = bfs(cx, cy, cn, board)
-        ans += fx
-        # for x in range(1, R+1):
-        #     for y in range(1, C+1):
-        #         if door[x][y]:
-        #             print("*", end=" ")
-        #         else:
-        #             print(board[x][y], end=" ")
-        #     print("")
-        # print("-----")
-        # print(fx, fy)
+        golems_on_board.append(golems[i])
+        golems[i].add_on_board(board, door, i+1)
+
+        ans += bfs(golems[i].cx, golems[i].cy, board, door)
+
 print(ans)
