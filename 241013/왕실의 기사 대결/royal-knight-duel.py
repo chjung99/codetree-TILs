a0ccee3064d30a517de3cfc03ada5fb7aea38371
chjung99 <62923434+chjung99@users.noreply.github.com>
2024-10-13 11:00:52 +0900

    Title: 왕실의 기사 대결, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241013/왕실의 기사 대결/royal-knight-duel.py
+++ 241013/왕실의 기사 대결/royal-knight-duel.py
@@ -1,98 +1,115 @@
+from copy import deepcopy
 from collections import deque
 
-# 전역 변수들을 정의합니다.
-MAX_N = 31
-MAX_L = 41
+
+class Knight:
+    def __init__(self, x, y, h, w, k):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.w = w
+        self.k = k
+        self.damage = 0
+        self.points = [[i, j] for i in range(x, x + h) for j in range(y, y + w)]
+
+    def move(self, d):
+        self.x, self.y = self.x + dx[d], self.y + dy[d]
+        for i in range(len(self.points)):
+            self.points[i][0] = self.points[i][0] + dx[d]
+            self.points[i][1] = self.points[i][1] + dy[d]
+
+    def count_trap(self, board):
+        cnt = 0
+        for x, y in self.points:
+            if board[x][y] == 1:
+                cnt += 1
+        return cnt
+
+
+L, N, Q = map(int, input().split())
+board = []
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+knight_list = []
+ans = 0
 
-info = [[0 for _ in range(MAX_L)] for _ in range(MAX_L)]
-bef_k = [0 for _ in range(MAX_N)]
-r = [0 for _ in range(MAX_N)]
-c = [0 for _ in range(MAX_N)]
-h = [0 for _ in range(MAX_N)]
-w = [0 for _ in range(MAX_N)]
-k = [0 for _ in range(MAX_N)]
-nr = [0 for _ in range(MAX_N)]
-nc = [0 for _ in range(MAX_N)]
-dmg = [0 for _ in range(MAX_N)]
-is_moved = [False for _ in range(MAX_N)]
+for _ in range(L):
+    board.append(list(map(int, input().split())))
+
+for _ in range(N):
+    r, c, h, w, k = map(int, input().split())
+    r -= 1
+    c -= 1
+    knight_list.append(Knight(r, c, h, w, k))
 
 
-# 움직임을 시도해봅니다.
-def try_movement(idx, dir):
-    q = deque()
-    is_pos = True
+def out_of_range(x, y):
+    return x < 0 or x >= L or y < 0 or y >= L
 
-    # 초기화 작업입니다.
-    for i in range(1, n + 1):
-        dmg[i] = 0
-        is_moved[i] = False
-        nr[i] = r[i]
-        nc[i] = c[i]
 
-    q.append(idx)
-    is_moved[idx] = True
+def include_blocked(points):
+    for x, y in points:
+        if out_of_range(x, y) or board[x][y] == 2:
+            return True
+    return False
 
-    while q:
-        x = q.popleft()
 
-        nr[x] += dx[dir]
-        nc[x] += dy[dir]
+for _ in range(Q):
+    i, d = map(int, input().split())
+    i -= 1
+    if knight_list[i].k <= 0:
+        continue
+    copy_knight_list = [row[:] for row in knight_list]
+    knight = copy_knight_list[i]
+    knight.move(d)
+    is_blocked = False
+    if include_blocked(knight.points):
+        continue
+    queue = deque([knight])
+    moved_knight_list = {knight}
+    while queue:
+        if is_blocked:
+            break
+        cur_knight = queue.popleft()
+        for x, y in cur_knight.points:
+            for another_knight in copy_knight_list:
+                if another_knight.k <= 0:
+                    continue
+                if cur_knight == another_knight:
+                    continue
+                if [x, y] in another_knight.points:
+                    queue.append(another_knight)
+                    another_knight.move(d)
+                    moved_knight_list.add(another_knight)
+                    if include_blocked(another_knight.points):
+                        is_blocked = True
+                        break
 
-        # 경계를 벗어나는지 체크합니다.
-        if nr[x] < 1 or nc[x] < 1 or nr[x] + h[x] - 1 > l or nc[x] + w[x] - 1 > l:
-            return False
-
-        # 대상 조각이 다른 조각이나 장애물과 충돌하는지 검사합니다.
-        for i in range(nr[x], nr[x] + h[x]):
-            for j in range(nc[x], nc[x] + w[x]):
-                if info[i][j] == 1:
-                    dmg[x] += 1
-                if info[i][j] == 2:
-                    return False
-
-        # 다른 조각과 충돌하는 경우, 해당 조각도 같이 이동합니다.
-        for i in range(1, n + 1):
-            if is_moved[i] or k[i] <= 0:
+            if is_blocked:
+                break
+    if not is_blocked:
+        for knight in moved_knight_list:
+            if copy_knight_list[i] == knight:
                 continue
-            if r[i] > nr[x] + h[x] - 1 or nr[x] > r[i] + h[i] - 1:
-                continue
-            if c[i] > nc[x] + w[x] - 1 or nc[x] > c[i] + w[i] - 1:
-                continue
+            damage = knight.count_trap(board)
+            knight.k -= damage
+            knight.damage += damage
+        knight_list = [row[:] for row in copy_knight_list]
 
-            is_moved[i] = True
-            q.append(i)
-
-    dmg[idx] = 0
-    return True
-
-
-# 특정 조각을 지정된 방향으로 이동시키는 함수입니다.
-def move_piece(idx, move_dir):
-    if k[idx] <= 0:
-        return
-
-    # 이동이 가능한 경우, 실제 위치와 체력을 업데이트합니다.
-    if try_movement(idx, move_dir):
-        for i in range(1, n + 1):
-            r[i] = nr[i]
-            c[i] = nc[i]
-            k[i] -= dmg[i]
-
-
-# 입력값을 받습니다.
-l, n, q = map(int, input().split())
-for i in range(1, l + 1):
-    info[i][1:] = map(int, input().split())
-for i in range(1, n + 1):
-    r[i], c[i], h[i], w[i], k[i] = map(int, input().split())
-    bef_k[i] = k[i]
-
-for _ in range(q):
-    idx, d = map(int, input().split())
-    move_piece(idx, d)
-
-# 결과를 계산하고 출력합니다.
-ans = sum([bef_k[i] - k[i] for i in range(1, n + 1) if k[i] > 0])
+for knight in knight_list:
+    if knight.k > 0:
+        ans += knight.damage
 print(ans)
+
+# 4 3 4
+# 0 0 1 0
+# 0 0 1 0
+# 1 1 0 1
+# 0 0 1 0
+# 1 2 2 1 5
+# 2 1 2 1 1
+# 3 2 1 2 3
+# 1 2
+# 1 2
+# 3 3
+# 3 0
